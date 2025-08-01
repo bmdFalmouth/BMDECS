5eb10530d868aec1aa0a19f65e475ab9d8b0b004
Brian McDonald <brian.mcdonald@falmouth.ac.uk>
2025-08-01 08:45:05 +0100

    Initial Entity

--- src/lib/entity.cpp
+++ src/lib/entity.cpp
@@ -1,1 +1,15 @@
 #include <entity.hpp>
+
+EntityManager::EntityManager()
+{
+    m_NumberOfLivingEntities=0;
+    for (Entity id=0;id<MAX_ENTITIES;id++)
+    {
+        m_AvailableEntities.push(id);
+    }
+}
+
+EntityManager::~EntityManager()
+{
+
+}
--- src/lib/include/entity.hpp
+++ src/lib/include/entity.hpp
@@ -1,6 +1,20 @@
-#include  <inttypes.h>
+//https://austinmorlan.com/posts/entity_component_system/
+
+#include <inttypes.h>
+#include <array>
+#include <queue>
 
 using Entity = uint32_t;
 
 const Entity MAX_ENTITIES = 10000;
+
+class EntityManager
+{
+    public:
+        EntityManager();
+        ~EntityManager();
+    private:
+        std::queue<Entity> m_AvailableEntities;
+        uint32_t m_NumberOfLivingEntities;
+};
 
